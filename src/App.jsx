c3a6dab22db60a725e202065fde22b8f691f8c7d
sailecodes <eliasiv.codes@gmail.com>
2023-12-01 13:04:08 -0800

    add default layout with navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Projects from "./components/pages/Projects";
+import Layout from "./components/pages/Layout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/projects",
-    element: <Projects />,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/projects",
+        element: <Projects />,
+      },
+    ],
   },
 ]);
 
