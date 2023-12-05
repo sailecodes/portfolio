6208c663e7edd4a4793479a6edce95c6c060ec59
saile <eliasiv.codes@gmail.com>
2023-12-04 17:38:16 -0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,29 @@ import Projects from "./components/pages/Projects";
 import Layout from "./components/pages/Layout";
 import Contact from "./components/pages/Contact";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/projects",
-        element: <Projects />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "/projects",
+          element: <Projects />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+      ],
+    },
+  ],
+  { basename: "/portfolio" }
+);
 
 const App = () => {
   return <RouterProvider router={router} />;
