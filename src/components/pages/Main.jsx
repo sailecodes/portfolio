9c791621eced90d9432906670c90c024f38dcd4e
saile <eliasiv.codes@gmail.com>
2024-03-28 21:58:44 -0700

    add initial impl. of responsive markers

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,47 +1,92 @@
-import { motion } from "framer-motion";
+import { motion, useAnimationControls, useInView } from "framer-motion";
 import { FaLinkedin, FaTelegramPlane, FaGithub } from "react-icons/fa";
 
 import "../../assets/sass/main.scss";
-import JavaIcon from "../icons/JavaIcon";
-import JavaScriptIcon from "../custom/icons/JavaScriptIcon";
-import HtmlIcon from "../icons/HtmlIcon";
-import CssIcon from "../icons/CssIcon";
-import ReactIcon from "../icons/ReactIcon";
-import NodeIcon from "../icons/NodeIcon";
 import { projects_new } from "../../data/projects";
+import { useEffect, useRef } from "react";
 
 const Main = () => {
+  const hero = useRef(null);
+  const about = useRef(null);
+  const projects = useRef(null);
+  const credit = useRef(null);
+
+  const heroInView = useInView(hero);
+  const aboutInView = useInView(about);
+  const projectsInView = useInView(projects);
+  const creditInView = useInView(credit, { amount: 0.25 });
+
   return (
-    <div className="main--container">
+    <main className="main">
       <nav className="main--nav">
-        {Array.from(Array(3)).map((_, ind) => (
+        <motion.div
+          className="main--nav-marker"
+          whileHover={{ scale: 1.4, cursor: "pointer" }}
+          transition={{ duration: 0.3 }}
+          style={{ backgroundColor: heroInView ? "#9c9faa" : "#202740" }}
+        />
+        <motion.div
+          className="main--nav-marker"
+          whileHover={{ scale: 1.4, cursor: "pointer" }}
+          transition={{ duration: 0.3 }}
+          style={{ backgroundColor: !heroInView && aboutInView ? "#9c9faa" : "#202740" }}
+        />
+        <motion.div
+          className="main--nav-marker"
+          whileHover={{ scale: 1.4, cursor: "pointer" }}
+          transition={{ duration: 0.3 }}
+          style={{ backgroundColor: !aboutInView && projectsInView && !creditInView ? "#9c9faa" : "#202740" }}
+        />
+        <motion.div
+          className="main--nav-marker"
+          whileHover={{ scale: 1.4, cursor: "pointer" }}
+          transition={{ duration: 0.3 }}
+          style={{ backgroundColor: creditInView ? "#9c9faa" : "#202740" }}
+        />
+        {/* {Array.from(Array(4)).map((_, ind) => (
           <motion.div
             key={ind}
-            whileHover={{ scale: 1.2, cursor: "pointer" }}
-            transition={{ duration: 0.3 }}
             className="main--nav-marker"
+            whileHover={{ scale: 1.4, cursor: "pointer", color: "#e6e7ea" }}
+            transition={{ duration: 0.3 }}
           />
-        ))}
+        ))} */}
       </nav>
-      <main className="main--content">
-        <section className="main--hero">
+      <div>
+        <section
+          className="main--hero"
+          ref={hero}>
           <p>Hi, I&apos;m Elias 👋</p>
           <p>an aspiring software engineer with a passion to use software as a medium to help others.</p>
           <div>
-            <FaLinkedin size={22} />
-            <FaGithub size={22} />
+            <a
+              href="https://www.linkedin.com/in/elias-iv-roman/"
+              target="_blank"
+              rel="noopener noreferrer">
+              <FaLinkedin size={22} />
+            </a>
+            <a
+              href="https://github.com/sailecodes"
+              target="_blank"
+              rel="noopener noreferrer">
+              <FaGithub size={22} />
+            </a>
           </div>
         </section>
-        <section className="main--about">
+        <section
+          className="main--about"
+          ref={about}>
           <header className="main--section-header">About me</header>
           <p>
             I&apos;m a recent CS graduate from UC San Diego with a firm understanding of fundamental CS principles, such
-            as algorithms, data structures, and software development practices. Moreover, I have adequate experience in
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi aliquam enim commodi omnis animi rerum ipsum!
-            Nam debitis magni vitae id. Vitae dicta iste cum porro, perferendis quos molestias modi.
+            as algorithms, data structures, and software development practices. Moreover, I have extensive experience in
+            code documentation, version control, collaborative team projects, and several languages, such as Java and
+            JavaScript. I&apos;ve also tackled several personal projects that focus on full-stack development.
           </p>
         </section>
-        <section className="main--projects">
+        <section
+          className="main--projects"
+          ref={projects}>
           <header className="main--section-header">Projects</header>
           <div>
             {projects_new.map((project, ind) => (
@@ -57,8 +102,22 @@ const Main = () => {
             ))}
           </div>
         </section>
-      </main>
-    </div>
+        <section
+          className="main--credit"
+          ref={credit}>
+          <header className="main--section-header">Credit</header>
+          Inspired by Brittany Chiang&apos;s beautiful{" "}
+          <a
+            href="https://brittanychiang.com/?ref=hackernoon.com"
+            target="_blank"
+            rel="noopener noreferrer">
+            portfolio
+          </a>
+          . Built with <span>React.js</span>, designed with <span>Sass</span>, animated with <span>Framer Motion</span>,
+          and deployed on <span>Netlify</span>.
+        </section>
+      </div>
+    </main>
   );
 };
 
@@ -90,8 +149,8 @@ const Project = ({ date, title, links, intro, description, skills }) => {
         {description}
       </p>
       <div className="main--projects-project-skills">
-        {skills.map((skill) => (
-          <p>{skill}</p>
+        {skills.map((skill, ind) => (
+          <p key={`${title} ${skill} ${ind}`}>{skill}</p>
         ))}
       </div>
     </div>
