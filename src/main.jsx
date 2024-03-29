c08d220fd20b46ea9c34659b14f0656eb81bea7a
saile <eliasiv.codes@gmail.com>
2024-03-28 22:10:32 -0700

    design mobile layout

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./assets/sass/_default.scss";
+import "./index.css";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
