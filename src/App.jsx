14bde14e2f3ae2b4cc7927901a52389e5d97cce0
saile <eliasiv.codes@gmail.com>
2024-03-29 18:16:41 -0700

    build

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,10 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./components/pages/Home";
-import Projects from "./components/pages/Projects";
-import Layout from "./components/pages/Layout";
-import Contact from "./components/pages/Contact";
+
 import Main from "./components/pages/Main";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/projects",
-        element: <Projects />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-    ],
-  },
-  {
-    path: "/test",
     element: <Main />,
   },
 ]);
