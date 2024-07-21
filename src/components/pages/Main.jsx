c084e0ed7e1a9c7925bf9e40748764fc2c74a0e6
saile <eliasiv.codes@gmail.com>
2024-07-20 21:07:12 -0700

    build

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -3,6 +3,7 @@ import { FaLinkedin, FaTelegramPlane, FaGithub } from "react-icons/fa";
 import "../../assets/sass/main.scss";
 import catGif from "../../assets/gifs/cat-gif.gif";
 import projects from "../../data/projects";
+import experience from "../../data/experience";
 
 const Main = () => {
   return (
@@ -14,16 +15,10 @@ const Main = () => {
             an aspiring software engineer with a passion to use software as a medium to help others.
           </p>
           <div className="main--hero-links">
-            <a
-              href="https://www.linkedin.com/in/elias-iv-roman/"
-              target="_blank"
-              rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/elias-iv-roman/" target="_blank" rel="noopener noreferrer">
               <FaLinkedin size={23} />
             </a>
-            <a
-              href="https://github.com/sailecodes"
-              target="_blank"
-              rel="noopener noreferrer">
+            <a href="https://github.com/sailecodes" target="_blank" rel="noopener noreferrer">
               <FaGithub size={23} />
             </a>
           </div>
@@ -31,8 +26,8 @@ const Main = () => {
         <section className="main--about">
           <header className="main--section-header">About me</header>
           <p>
-            I&apos;m a recent CS graduate from UC San Diego with a firm understanding of fundamental CS principles, such
-            as <span>algorithms</span>, <span>data structures</span>, and <span>software development </span>
+            I&apos;m a CS graduate from UC San Diego with a firm understanding of fundamental CS principles, such as{" "}
+            <span>algorithms</span>, <span>data structures</span>, and <span>software development </span>
             practices. Moreover, I&apos;ve extensive experience in <span>code documentation</span>,
             <span> version control</span>, <span>collaborative team projects</span>, <span>agile methodologies</span>,
             and several languages, such as
@@ -41,26 +36,38 @@ const Main = () => {
           <p>
             I&apos;ve also tackled various personal projects that focus on <span>full-stack development</span>. Most are
             built using the <span>MERN Stack</span>, in which I use multiple frameworks and packages, including
-            <span> React Query</span>, <span>React Router Dom</span>, and <span>Sass</span>.
+            <span> React Query</span>, <span>React Router Dom</span>, and <span>Sass</span>. In addition, I&apos;ve
+            hosted <span>Postgres</span> databases on Railway, especially for my project Shoe Szn.
           </p>
           <p>
             When it comes to non-techy things, I enjoy <span>eating food</span>, <span>playing volleyball</span>, and
             <span> spending time with friends and family</span>. At home, I find comfort in my
             <span> adorable, shy cat</span> named Nori!
           </p>
-          <img
-            src={catGif}
-            alt="Animated cat massaging another cat"
-          />
+          <img src={catGif} alt="Animated cat massaging another cat" />
         </section>
       </div>
       <div className="main--right-half">
+        <section className="main--experience">
+          <header className="main--section-header">Experience</header>
+          <div>
+            {experience.map((exp, ind) => (
+              <Experience
+                key={exp.description + ind}
+                date={exp.date}
+                position={exp.position}
+                company={exp.company}
+                description={exp.description}
+              />
+            ))}
+          </div>
+        </section>
         <section className="main--projects">
           <header className="main--section-header">Projects</header>
           <div>
             {projects.map((project, ind) => (
               <Project
-                key={ind}
+                key={project.description + ind}
                 date={project.date}
                 title={project.title}
                 links={project.links}
@@ -74,10 +81,7 @@ const Main = () => {
         <section className="main--credit">
           <header className="main--section-header">Credit</header>
           Inspired by Brittany Chiang&apos;s beautiful{" "}
-          <a
-            href="https://brittanychiang.com/?ref=hackernoon.com"
-            target="_blank"
-            rel="noopener noreferrer">
+          <a href="https://brittanychiang.com/?ref=hackernoon.com" target="_blank" rel="noopener noreferrer">
             portfolio
           </a>
           . Built with <span>React.js</span>, designed with <span>Sass</span>, and deployed on <span>Netlify</span>.
@@ -94,17 +98,11 @@ const Project = ({ date, title, links, intro, description, skills }) => {
       <div className="main--projects-project-title">
         <p>{title}</p>
         <div>
-          <a
-            href={links[0]}
-            target="_blank"
-            rel="noopener noreferrer">
+          <a href={links[0]} target="_blank" rel="noopener noreferrer">
             <FaGithub size={23} />
           </a>
           {links[1] !== "#" && (
-            <a
-              href={links[1]}
-              target="_blank"
-              rel="noopener noreferrer">
+            <a href={links[1]} target="_blank" rel="noopener noreferrer">
               <FaTelegramPlane size={23} />
             </a>
           )}
@@ -119,6 +117,16 @@ const Project = ({ date, title, links, intro, description, skills }) => {
           <p key={`${title} ${skill} ${ind}`}>{skill}</p>
         ))}
       </div>
+    </div>
+  );
+};
+
+const Experience = ({ date, position, company, description }) => {
+  return (
+    <div className="main--experience-experience">
+      <p className="main--experience-experience-date">{date}</p>
+      <p className="main--experience-experience-position">{`${position}, ${company}`}</p>
+      <p className="main--experience-experience-description">{description}</p>
     </div>
   );
 };
---
+++ src/data/experience.js
@@ -0,0 +1,18 @@
+const experience = [
+  {
+    date: "Present",
+    position: "IT Specialist and Electrician",
+    company: "Power Communications",
+    description:
+      "Ran and terminated network cables and fiber for multiple data and access points, installed an Algo intercom system with 8198, 1198, 8196, and 1196 speakers controlled by an 8103 pager and accessed through Zoom VoIP, configured a server composed of a virtual domain controller, data server and application server.",
+  },
+  {
+    date: "June 2022 - September 2022",
+    position: "Cellular Distributed Antenna System Installer",
+    company: "Power Communications",
+    description:
+      "Spliced and tested hundreds of fiber, installed fiber cassettes and radios, and connected radios to hubs composed of several fiber network terminals -- all in Crypto.com Arena.",
+  },
+];
+
+export default experience;
